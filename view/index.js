fb2cb1d50e577a02c160e4fc813978446eb53717
pnlybubbles <pnlybubbles@gmail.com>
2017-03-20 19:10:17 +0900

    feat: add menus

--- view/index.js
+++ view/index.js
@@ -29,12 +29,22 @@ module.exports = function mainView (state, prev, send) {
           </div>
         </div>
         <div class="cover">
+          <nav class="menu">
+            <ul class="list">
+              <li class="item"><a href="#about">About</a></li>
+              <li class="item"><a href="#location">Location</a></li>
+              <li class="item"><a href="#schedule">Schedule</a></li>
+              <li class="item"><a href="#groups">Groups</a></li>
+            </ul>
+          </nav>
+          <div class="pic"></div>
           <div class="names">
             <div class="name first">応用</div>
             <div class="name second">数学</div>
             <div class="name third">研究部</div>
             <div class="name fourth">東京理科大学</div>
           </div>
+          <div class="arrow"></div>
         </div>
       </div>
     </main>
