3f5cd35186701b377cb75cb00051f2bbe57e5813
pnlybubbles <pnlybubbles@gmail.com>
2017-04-06 19:39:39 +0900

    feat: add contact page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(require('./src/reducer/loader'))
 app.use(require('./src/reducer/page'))
 
 app.route('/', require('./view/index'))
+app.route('/contact', require('./view/contact'))
 app.route('/page/*', require('./view/page'))
 app.route('/404', require('./view/404'))
 
---
+++ view/contact.js
@@ -0,0 +1,32 @@
+const html = require('choo/html')
+const css = require('scopedify')
+const CONST = require('../src/constants')
+const header = require('./components/header')
+
+const scope = css('./contact')
+
+module.exports = function contactView (state, emit) {
+  return scope(html`
+    <body class="root">
+      ${header(html`
+        <i class="fa fa-comments" aria-hidden="true"></i>
+      `)(...arguments)}
+      <section class="content">
+        <h1>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <span>Twitter</span>
+        </h1>
+        <p>
+          <a href=${CONST.TWITTER_URL}>@${CONST.TWITTER_SCREEN_NAME}</a>
+        </p>
+        <h1>
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <span>Mail</span>
+        </h1>
+        <p>
+          <a href="mailto:${CONST.MAIL}">${CONST.MAIL}</a>
+        </p>
+      </section>
+    </body>
+  `)
+}
