6def7e13255073786b7cf1fbb4001a19174466de
ｱﾗｲｸﾞﾏ <akihiko.arai@amail.plala.or.jp>
2018-03-25 23:05:06 +0900

    Update menu.js

--- view/components/menu.js
+++ view/components/menu.js
@@ -8,10 +8,9 @@ module.exports = function menu (state, emit) {
   return scope(html`
     <ul class="root">
       <li class="item"><a href="#page/main/about">About</a></li>
-      <li class="item"><a href="#page/main/location">Location</a></li>
       <li class="item"><a href="#page/main/schedule">Schedule</a></li>
       <li class="item"><a href="#archive">Archive</a></li>
-      <li class="item"><a href=${config.TWITTER_URL}>Twitter</a></li>
+      <li class="item"><a href="#page/main/#contact">Contact</a></li>
     </ul>
   `)
 }
