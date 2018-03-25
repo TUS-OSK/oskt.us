9a6f6edcd1687e4a70c0d22bf86039fbf10b5b22
ｱﾗｲｸﾞﾏ <akihiko.arai@amail.plala.or.jp>
2018-03-25 22:52:03 +0900

    Update contact.js

--- view/contact.js
+++ view/contact.js
@@ -15,6 +15,12 @@ module.exports = function contactView (state, emit) {
       `)(...arguments)}
       <section class="content">
         <h1>
+          Location
+        </h1>
+        <p>
+          部室: 2号館 5階 2507教室
+        </p>
+        <h1>
           <i class="fa fa-twitter" aria-hidden="true"></i>
           <span>Twitter</span>
         </h1>
