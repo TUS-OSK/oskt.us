1d3fa53437aaa811366dfc75eefdb9cb8f06340e
pnlybubbles <pnlybubbles@gmail.com>
2018-04-04 01:01:39 +0900

    画面遷移時にスクロール位置が最上部に戻らない問題を修正

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(log())
 
 app.use(require('./src/reducer/loader'))
 app.use(require('./src/reducer/page'))
+app.use(require('./src/reducer/fix-scroll'))
 
 app.route('/', require('./view/index'))
 app.route('/contact', require('./view/contact'))
