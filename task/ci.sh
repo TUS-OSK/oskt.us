b2fcd6c813ac13d42763542d301873fdb4e1efe5
Lucciola <clockvoid@users.noreply.github.com>
2018-09-23 10:24:36 +0900

    Update CircleCI Settings (#56)
    * Update circleci.yml
    
    * Fix yml file name for circleci
    
    * Fix docker image name
    
    * Fix docker image name
    
    * Fix npm version
    
    * Fix docker image version
    
    * Fix docker image version
    
    * Delete npm version up
    
    * Fix environment valuable
    
    * load bashrc in ci.sh
    
    * Fix shell to bash
    
    * Fix shell to bash
    
    * Fix load bashrc on ci.sh
    
    * Fix name of run
    
    * Add ignore branches
    
    * Fix environment valuable
    
    * Fix workflow
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix ignoring branch
    
    * Fix environment valuable
    
    * fix: ignore branch settings
    
    * delete: .python-version
    
    * fix: build runs only when branch is master
    
    * fix: build runs only when branch is master
    
    * fix: build runs only when branch is master
    
    * fix: buils runs only when branch is master
    
    * fix: buils runs only when branch is master
    
    * test: remove docker
    
    * fix: codingstyle
    
    * fix: circleci/config.yml
    
    * fix: filters in config.yml
    
    * delete: unrelated line
    
    * delete: old circle.yml
    
    * fix: .circleci/config.yml
    
    * fix: delete unrelated line in config.yml
    
    * fix: position of version number
    
    * fix: position of version number
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * fix: config.yml
    
    * done: fix config.yml
    
    * fix: workflow name

--- task/ci.sh
+++ task/ci.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-git config --global user.name ${GIT_NAME}
-git config --global user.email ${GIT_EMAIL}
+#!/bin/bash
+source ~/.bashrc
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
 if git branch | grep -w gh-pages > /dev/null 2>&1; then git branch -D gh-pages; fi
 git checkout --orphan gh-pages
 npm run clean
@@ -8,7 +9,7 @@ npm run build
 ls -a | grep -v -E "^(\.|\.\.|\.git|public)$" | xargs rm -rf
 mv ./public/* ./
 rm -rf ./public
-if [ ! -z ${CNAME} ]; then echo -n ${CNAME} > CNAME; fi
+if [ ! -z $CNAME ]; then echo -n $CNAME > CNAME; fi
 git add -A
 git commit -m "update [skip ci]"
-git push -f ${GIT_REPO} gh-pages
+git push -f $GIT_REPO gh-pages
