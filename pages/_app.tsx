4be7d5502c81b15b6e453dffd84d4740f2f58224
momoogles <mo1419091@gmail.com>
2020-10-30 23:59:22 +0900

    グローバルスタイルを追加した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { createGlobalStyle } from 'styled-components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+
+const GlobalStyle = createGlobalStyle`
+  // NOTE: 初期スタイルの上書き
+  body {
+    margin: 0;
+  }
+`
