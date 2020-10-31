2382cfbb9887a4714851e141b82cee0116cf7b87
momoogles <mo1419091@gmail.com>
2020-10-31 12:10:56 +0900

    fontのスタイルを追加した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,7 +12,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 export default MyApp
 
+const fontSettings = css`
+  @font-face {
+    font-family: 'quasimoda';
+    src: url('/fonts/quasimoda-light.otf');
+  }
+`
+
 const GlobalStyle = createGlobalStyle`
+  ${fontSettings}
+
+  body {
+    font-family: 'quasimoda', '游ゴシック体', 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', 'メイリオ', Meiryo, Osaka, 'MS Pゴシック', 'MS PGothic', sans-serif;
+  }
+
   // NOTE: 初期スタイルの上書き
   body {
     margin: 0;
