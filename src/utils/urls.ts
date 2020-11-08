a6fe3e691c39aeee850cf8e46d4b076bcf4ae0e4
tomon9086 <tomo.mouse.9086@gmail.com>
2020-11-08 15:02:59 +0900

    関数化した

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,22 +3,24 @@ const externalUrlOrigin = {
   github: 'https://github.com',
 }
 
+const join = (...paths: string[]) => paths.join('/')
+
 type ExternalUrlType = keyof typeof externalUrlOrigin
 
-export const externalTo = (type: ExternalUrlType, path: string) => [externalUrlOrigin[type], path].join('/')
+export const externalTo = (type: ExternalUrlType, path: string) => join(externalUrlOrigin[type], path)
 
 const REPOSITORY_PATH = 'TUS-OSK/oskt.us'
 
 export const urlPageMdEdit = (slug: string) =>
-  externalTo('github', [REPOSITORY_PATH, 'edit', 'master', '_pages', 'markdowns', `${slug}.md`].join('/'))
+  externalTo('github', join(REPOSITORY_PATH, 'edit', 'master', '_pages', 'markdowns', `${slug}.md`))
 
 export const urlArticleMdEdit = (year: string, slug: string) =>
-  externalTo('github', [REPOSITORY_PATH, 'edit', 'master', '_articles', 'markdowns', year, `${slug}.md`].join('/'))
+  externalTo('github', join(REPOSITORY_PATH, 'edit', 'master', '_articles', 'markdowns', year, `${slug}.md`))
 
 export const mailTo = (to: string) => `mailto:${to}`
 
-export const articleTo = (year: string, slug: string) => ['/', 'articles', year, slug].join('/')
+export const articleTo = (year: string, slug: string) => join('/', 'articles', year, slug)
 
 export type PageType = 'about' | 'archive' | 'contact' | 'faq' | 'top' | 'news' | 'schedule'
 
-export const pageTo = (page: PageType) => ['/', page].join('/')
+export const pageTo = (page: PageType) => join('/', page)
