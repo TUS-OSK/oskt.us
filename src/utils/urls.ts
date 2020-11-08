0ce4ffff036c1b5b07a3bfee60d1dc5677addc3c
tomon9086 <tomo.mouse.9086@gmail.com>
2020-11-08 14:50:25 +0900

    :bug: 絶対パスのはずが相対パスとして解釈される問題

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,5 +1,3 @@
-import { join } from 'path'
-
 const externalUrlOrigin = {
   twitter: 'https://twitter.com',
   github: 'https://github.com',
@@ -7,20 +5,20 @@ const externalUrlOrigin = {
 
 type ExternalUrlType = keyof typeof externalUrlOrigin
 
-export const externalTo = (type: ExternalUrlType, path: string) => join(externalUrlOrigin[type], path)
+export const externalTo = (type: ExternalUrlType, path: string) => [externalUrlOrigin[type], path].join("/")
 
 const REPOSITORY_PATH = 'TUS-OSK/oskt.us'
 
 export const urlPageMdEdit = (slug: string) =>
-  externalTo('github', join(REPOSITORY_PATH, 'edit', 'master', '_pages', 'markdowns', `${slug}.md`))
+  externalTo('github', [REPOSITORY_PATH, 'edit', 'master', '_pages', 'markdowns', `${slug}.md`].join("/"))
 
 export const urlArticleMdEdit = (year: string, slug: string) =>
-  externalTo('github', join(REPOSITORY_PATH, 'edit', 'master', '_articles', 'markdowns', year, `${slug}.md`))
+  externalTo('github', [REPOSITORY_PATH, 'edit', 'master', '_articles', 'markdowns', year, `${slug}.md`].join("/"))
 
-export const mailTo = (to: string) => join('mailto:', to)
+export const mailTo = (to: string) => `mailto:${to}`
 
-export const articleTo = (year: string, slug: string) => join('/', 'articles', year, slug)
+export const articleTo = (year: string, slug: string) => ['/', 'articles', year, slug].join("/")
 
 export type PageType = 'about' | 'archive' | 'contact' | 'faq' | 'top' | 'news' | 'schedule'
 
-export const pageTo = (page: PageType) => join('/', page)
+export const pageTo = (page: PageType) => ['/', page].join("/")
