ef496b5917e81f6126e15bf67ff41905c706a264
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-02-20 16:04:02 +0900

    縮めた

--- src/pages/Home/ScheduleView/Calendar/index.tsx
+++ src/pages/Home/ScheduleView/Calendar/index.tsx
@@ -1,83 +1,70 @@
-import React, { ReactElement } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { EventCalendar, Events } from 'src/pages/Home/ScheduleView'
-import { EVENT_ELEMENT_HEIGHT } from './EventElement'
+import { EventCalendar } from 'src/pages/Home/ScheduleView'
 import MonthEventElement from './MonthEventElement'
 
 interface Props {
   eventCalendar: EventCalendar
 }
 
-const toReactElements = (months: number[], _events: Events) => {
-  const results: ReactElement[] = []
-  months.forEach((m, i) => {
-    const events = _events[m]
-    if (!events) return
-    results.push(
-      <MonthEventElementWrapper index={i} key={`${m}`}>
-        <HoverActionController>
-          <MonthEventElement events={events} />
-        </HoverActionController>
-      </MonthEventElementWrapper>
-    )
-  })
-  return results
-}
-
 export default function Calendar({ eventCalendar: { months, events } }: Props) {
   return (
-    <Grid>
-      <Title>Month</Title>
-      <Title>Activities / Events</Title>
-
-      {months.map((m) => (
-        <Month key={m} month={m} />
-      ))}
-
-      <EventArea>{toReactElements(months, events)}</EventArea>
-    </Grid>
+    <Container>
+      <Header>
+        <MonthTitle>Month</MonthTitle>
+        <EventsTitle>Activities / Events</EventsTitle>
+      </Header>
+      {months.map((m) => {
+        const monthlyEvents = events[m]
+        return !monthlyEvents ? (
+          <Month key={m} month={m} />
+        ) : (
+          <Month key={m} month={m}>
+            <MonthEventElementWrapper>
+              <HoverActionController>
+                <MonthEventElement events={monthlyEvents} />
+              </HoverActionController>
+            </MonthEventElementWrapper>
+          </Month>
+        )
+      })}
+    </Container>
   )
 }
 
-const Grid = styled.div`
-  display: grid;
-  position: relative;
-  grid-template-columns: max-content 1fr;
-`
+const Container = styled.div``
 
-const Title = styled.div`
-  text-align: center;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
   padding: 8px;
 `
 
-const ROW_HEIGHT = EVENT_ELEMENT_HEIGHT + 8 * 2
+const MONTH_WIDTH = 80
+
+const MonthTitle = styled.div`
+  width: ${MONTH_WIDTH}px;
+`
+
+const EventsTitle = styled.div``
 
 const Month = styled.div<{ month: number }>`
   font-size: 16px;
   box-sizing: border-box;
-  height: ${ROW_HEIGHT}px;
+  min-height: 24px;
   border-top: 1px solid #ddd;
-  padding: 4px 6px;
-  grid-column: 1/3;
+  padding: 8px 6px;
+  display: flex;
 
   &::before {
     display: block;
+    width: ${MONTH_WIDTH}px;
     content: '${(p) => p.month}';
   }
 `
 
-const EventArea = styled.div`
-  grid-column: 2/3;
-  grid-row: 2;
-  position: relative;
-`
-
-const MonthEventElementWrapper = styled.div<{ index: number }>`
-  position: absolute;
-  padding: 8px;
-  width: 100%;
-  box-sizing: border-box;
-  transform: translateY(${(props) => ROW_HEIGHT * props.index}px);
+const MonthEventElementWrapper = styled.div`
+  flex: 1;
 `
 
 const HoverActionController = styled.div`
