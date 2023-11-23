aa7990dc1600063ed7e814885985b28b1cd80776
qq8244353 <qq8244353@users.noreply.github.com>
2023-11-23 18:36:26 +0000

    deploy: 47c60602f209ed9d672852605c5efe862a58a5aa

---
+++ _next/static/64gX8BL9V5rhkTSzCpVCs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fcontact","\u002Ffaq","\u002Farchive","\u002Fschedule","\u002Fnews","\u002Farticles\u002F[year]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
