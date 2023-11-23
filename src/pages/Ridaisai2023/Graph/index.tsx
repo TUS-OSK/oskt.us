570c7e8847d216c9a2e46b87b516e9526e4f6666
qq8244353 <qqq000a@outlook.com>
2023-11-24 02:59:07 +0900

    fix

--- src/pages/Ridaisai2023/Graph/index.tsx
+++ src/pages/Ridaisai2023/Graph/index.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import React from 'react'
-// import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 const dummyData = [
   {
