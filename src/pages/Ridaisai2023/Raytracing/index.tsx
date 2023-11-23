bdd5ddd60a1b99bfeb06a771a99136cb3aa1c936
8picoz <8picomu@gmail.com>
2023-11-23 18:38:06 +0900

    レイトレ班の説明追加

--- src/pages/Ridaisai2023/Raytracing/index.tsx
+++ src/pages/Ridaisai2023/Raytracing/index.tsx
@@ -4,27 +4,17 @@ export default function Raytracing() {
   return (
     <div>
       <Title>レイトレーシング班</Title>
-      <Description>
-        レイトレーシングとは光を現実と同じように計算することでとてもリアルなCGを作る手法です。映画などのリアルなCGには必ず使われていると言ってもの過言ではなく、近年ではゲームにも応用され始め、ますます注目度が高まっている技術です。
-        このサークルでは自作のレイトレーシングプログラムで実際に画像を出したり、新しい手法を調べて新しく実装したりなどレイトレーシングに関わる活動をしています。理大祭では活動で制作した画像をいくつか展示しています。
-      </Description>
-
-      <RayImages>
-        <div>
-          <RayImage1 />
-        </div>
-        <div>
-          <RayImage2 />
-        </div>
-      </RayImages>
-      <RayImages>
-        <div>
-          <RayImage3 />
-        </div>
-        <div>
-          <RayImage4 />
-        </div>
-      </RayImages>
+      <DescriptionBox>
+        <Description>
+          レイトレーシングとは光を現実と同じように計算することでとてもリアルなCGを作る手法です。
+          <br />
+          映画などのリアルなCGには必ず使われていると言ってもの過言ではなく、近年ではゲームにも応用され始め、ますます注目度が高まっている技術です。
+          <br />
+          このサークルでは自作のレイトレーシングプログラムで実際に画像を出したり、新しい手法を調べて新しく実装したりなどレイトレーシングに関わる活動をしています。
+          <br />
+          理大祭では活動で制作した画像をいくつか展示しています。
+        </Description>
+      </DescriptionBox>
     </div>
   )
 }
@@ -44,28 +34,41 @@ const Title = styled.h1`
   }
 `
 
-const Description = styled.div``
+const DescriptionBox = styled.div`
+  margin: 10%;
+  border-top: 2px solid #aaa;
+  border-bottom: 2px solid #aaa;
+  padding: 2em 0;
+  position: relative;
 
-const RayImages = styled.div`
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    top: -10px;
+    width: 2px;
+    height: -webkit-calc(100% + 20px);
+    height: calc(100% + 20px);
+    background-color: #aaa;
+  }
+
+  &::before {
+    left: 10px;
+  }
+
+  &::after {
+    right: 10px;
+  }
+`
+
+const Description = styled.div`
+  margin: 0 3%;
+  font-size: 1.2em;
+`
+
+const LoopSlide = styled.div`
+  position: relative;
   display: flex;
-`
-
-const RayImage1 = styled.div`
-  background-image: url('/images/ridaisai/2023/Raytracing/ray1.png');
-  max-width: 100%;
-`
-
-const RayImage2 = styled.div`
-  background-image: url('/images/ridaisai/2023/Raytracing/ray2.png');
-  max-width: 100%;
-`
-
-const RayImage3 = styled.div`
-  background-image: url('/images/ridaisai/2023/Raytracing/ray3.png');
-  max-width: 100%;
-`
-
-const RayImage4 = styled.div`
-  background-image: url('/images/ridaisai/2023/Raytracing/ray4.png');
-  max-width: 100%;
+  width: 100vw;
+  overflow: hidden;
 `
