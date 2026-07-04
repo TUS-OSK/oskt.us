86419eaec8743ebdaff33f04a36dc88cbdcb561e
Narfidort <Narfidort@users.noreply.github.com>
2026-07-04 03:16:11 +0000

    deploy: b5a9f5981ad5679c4cfdae478f98b30a43793d30

--- _next/static/WIGaSrWfXAmJZcI5Xli66/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fcontact","\u002Ffaq","\u002Farchive","\u002Fschedule","\u002F","\u002Fnews","\u002Farticles\u002F[year]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
